63e959c68fcbe7246614e1bd16fe5595e19cc619
Dorcas Olajide <dorcas_olajide@yahoo.com>
2022-02-28 17:42:18 +0100

    changes 2

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,6 @@ export const getStaticProps = async () => {
   };
 };
 
-// const apiKey = 'f69b33c1639044108e76fc692e1bd9d6'
-// const api = `https://newsapi.org/v2/everything?q=web&q=tech&language=en&apiKey=${apiKey}`
-// const fetchRes = await fetch(api);
-// const resJson = await fetchRes.json();
-// const articles = await resJson.articles;
-// articles.forEach((article, index) => article.id = index + 1)
-// console.log(articles)
-
 // export const getStaticProps = async () => {
 //   const res = await fetch(
 //     `https://jsonplaceholder.typicode.com/posts?_limit=8`
